a711100d373409f52edf40fb90eda9f139a9d9a1
kyle roux <kyle@level2designs.com>
2015-09-02 16:36:37 -0700

    h;

--- todoapp.py
+++ todoapp.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://patrycja:mypassword@localhost/todoapp'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
+app.debug = True
 from views import *
 
 
